f4c1a43b14ce276205b1d8a838a4ddf7291dabca
Martin Edenhofer <me@edenhofer.de>
2013-07-10 13:47:35 +0200

    Renamed file to avoid package conflict.

--- Custom/Kernel/Modules/CustomerTicketSearchSimple.pm
+++ Custom/Kernel/Modules/CustomerTicketSearchSimple.pm
@@ -1,13 +1,8 @@
 # --
-# Kernel/Modules/CustomerTicketSearch.pm - Utilities for tickets
-# Copyright (C) 2001-2013 OTRS AG, http://otrs.com/
-# --
-# This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# Kernel/Modules/CustomerTicketSearchSimple.pm - Utilities for tickets
 # --
 
-package Kernel::Modules::CustomerTicketSearch;
+package Kernel::Modules::CustomerTicketSearchSimple;
 
 use strict;
 use warnings;
