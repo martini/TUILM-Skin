2948e703979fe01f4d8e2318ed897db553ec954e
Alejandra Lopez <alejandra.lopez@tu-ilmenau.de>
2013-08-22 16:02:00 +0200

    faq und language

---
+++ Kernel/Language/de_UniRZ_Skin.pm
@@ -0,0 +1,15 @@
+
+package Kernel::Language::de_UniRZ_Skin;
+
+use strict;
+use warnings;
+
+sub Data {
+    my $Self = shift;
+
+    $Self->{Translation}->{'New ticket'} = 'Neues Ticket';
+    $Self->{Translation}->{'Create new ticket'} = 'Neues Ticket erstellen';
+
+}
+
+1;
